db81bd07151ef80dd7a727705a4067a44a524410
shichenai <shichen.py@gmail.com>
2019-08-23 14:25:22 +0900

    Revised some bugs.

--- video_capture.py
+++ video_capture.py
@@ -5,18 +5,25 @@ import os
 
 parser = argparse.ArgumentParser(description='Capture video')
 parser.add_argument('--camera_index', type=int, default=0, help='Camera index.')
-parser.add_argument('--save_path', type=str, default='output.avi', help='Save path.')
+parser.add_argument('--save_path', type=str, default='./data/output.avi', help='Save path.')
 parser.add_argument('--width', type=int, help='Video width.')
 parser.add_argument('--height', type=int, help='Video height.')
 parser.add_argument('--fps', type=int, default=30, help='Recording fps.')
 args = parser.parse_args()
 
 if __name__ == '__main__':
+
+    assert(len(args.save_path) > 0)
+    assert(args.save_path.fps > 0)
     cap = cv2.VideoCapture(args.camera_index)
     
     w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)) if args.width == None else args.width
     h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)) if args.height == None else args.height
     fourcc = cv2.VideoWriter_fourcc(*'XVID')   
+
+    file_dir = os.path.dirname(args.save_path)
+    if len(file_dir) > 0 and not os.path.exists(file_dir):
+        os.mkdir(file_dir)
 
     out = cv2.VideoWriter()
     out.open(args.save_path, fourcc, args.fps, (w, h), True)
