f983419851f6708ab23ff0ef934f02c1f37d74a9
Shi Chen <45330366+ShiChenAI@users.noreply.github.com>
2019-08-23 14:08:34 +0900

    Create video_capture.py

---
+++ video_capture.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import cv2
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Capture video')
+parser.add_argument('--camera_index', type=int, default=0, help='Camera index.')
+parser.add_argument('--save_path', type=str, default='output.avi', help='Save path.')
+parser.add_argument('--width', type=int, help='Video width.')
+parser.add_argument('--height', type=int, help='Video height.')
+parser.add_argument('--fps', type=int, default=30, help='Recording fps.')
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    cap = cv2.VideoCapture(args.camera_index)
+    
+    w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)) if args.width == None else args.width
+    h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)) if args.height == None else args.height
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')   
+
+    out = cv2.VideoWriter()
+    out.open(args.save_path, fourcc, args.fps, (w, h), True)
+
+    while True:
+        ret, frame=cap.read()
+        cv2.imshow('Results', frame)
+        out.write(frame)
+        if cv2.waitKey(1) & 0xFF==ord('q') or ret==False:
+            break
+
+    cap.release()
+    out.release()
